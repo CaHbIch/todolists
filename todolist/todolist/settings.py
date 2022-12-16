e7d5fb16a0f25a0bcc63cf8bf37d6d1d0a479a61
alexander <FILlove@mail.ru>
2022-12-16 23:06:47 +0300

    actoin

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
